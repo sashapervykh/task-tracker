4568c0b92a1f27a8dc8bd387d52386a757a1d465
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-26 21:24:50 +0300

    feat: add day title

--- index/index.js
+++ index/index.js
@@ -1,10 +1,16 @@
-function createDay() {
+function createDay(date) {
   const main = document.querySelector("main");
   const dayWrapper = document.createElement("section");
   dayWrapper.classList.add("day-wrapper");
   main.append(dayWrapper);
+  const dayDate = document.createElement("h2");
+  dayDate.textContent = date;
+  dayDate.classList.add("day-date");
+  dayDate.classList.add("bordered");
+  dayWrapper.append(dayDate);
 }
 
+createDay("Mo, 28.02.2024");
 createDay();
-
-console.log(1);
+createDay();
+createDay();
