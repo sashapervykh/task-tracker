868bfd25766d6b0aef86e9f119a83c548c0cc581
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-26 21:48:33 +0300

    feat: add line for adding tasks

--- index/index.js
+++ index/index.js
@@ -8,8 +8,20 @@ function createDay(date) {
   dayDate.classList.add("day-date");
   dayDate.classList.add("bordered");
   dayWrapper.append(dayDate);
+  createAddLine(dayWrapper);
 }
 
+function createAddLine(parent) {
+  const addLineWrapper = document.createElement("form");
+  parent.append(addLineWrapper);
+  addLineWrapper.classList.add("add-line");
+  const addingInput = document.createElement("input");
+  addLineWrapper.append(addingInput);
+  const addingButton = document.createElement("button");
+  addingButton.textContent = "ADD";
+  addingButton.classList.add("adding-button");
+  addLineWrapper.append(addingButton);
+}
 createDay("Mo, 28.02.2024");
 createDay();
 createDay();
