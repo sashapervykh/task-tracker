77e78bb40c7c5ffcfe1ef92c5535b7b2ef7e86df
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-26 20:50:45 +0300

    init: start to-do list

---
+++ index/index.js
@@ -0,0 +1,10 @@
+function createDay() {
+  const main = document.querySelector("main");
+  const dayWrapper = document.createElement("section");
+  dayWrapper.classList.add("day-wrapper");
+  main.append(dayWrapper);
+}
+
+createDay();
+
+console.log(1);
