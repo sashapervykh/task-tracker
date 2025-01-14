8f5c57b2bac56782bf2cc4d1697d570ba68f57c7
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-01-14 16:03:40 +0300

    feat: add webpack dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/script.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+module.exports = (env) => {
+  return {
+    mode: env.mode ?? "production",
+    entry: path.resolve(__dirname, "src", "script.js"),
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
+    ],
+    optimization: {
+      runtimeChunk: "single",
+    },
+  };
 };
